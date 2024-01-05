5a6cee462e77ad1bb93f502a879b96942986bc05
Rich Colburn <the3dadvantage@gmail.com>
2024-01-04 16:54:49 -0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -17,21 +17,31 @@ else:
     D_TYPE_I = np.int32
 
 
-psep = os.path.sep
-path = '/home/rich/Desktop/cloth/character_engine'
-sys.path.append(path)
-
-try:
-    C = bpy.data.texts['coordinates.py'].as_module()
-    
-except:
-    import coordinates as C
-    importlib.reload(C)
-
-
 #=======================#
 # PYTHON ---------------#
 #=======================#
+# python
+def code_line():
+    """Prints the line of code where it's called"""
+    frame = inspect.currentframe()
+    calling_line = frame.f_back.f_lineno
+    print("Called on line:", calling_line)
+
+
+# python
+def numpy_array_to_string(numpy_array):
+    return ','.join(map(str, numpy_array.ravel()))
+
+
+# python
+def string_to_numpy_array(string, shape=None, dtype=np.float32):
+    ar = np.fromstring(string, dtype=dtype, sep=',')
+    if shape is None:
+        shape = (ar.shape[0]//3, 3)
+    ar.shape = shape
+    return ar
+
+
 # python
 def fast_attribute(ob, obm=None):
     """Faster than list comprehension
@@ -59,6 +69,7 @@ def split_float(value):
     return int_part, decimal
 
 
+# python
 def r_print(r, m=''):
     """Prints numpy arrays
     and lists rounded off."""
@@ -69,6 +80,42 @@ def r_print(r, m=''):
             print(np.round(v, 3), m)
         return        
     print(np.round(r, 3), m)
+
+
+### ==================== ###
+#      VERTEX GROUPS       #
+### ==================== ###
+# vertex groups
+def assign_vert_group(ob, v_list, name, weight=1.0):
+    """Does what you might imagine
+    (makes me a sandwich)."""
+    if name not in ob.vertex_groups:
+        nvg = ob.vertex_groups.new(name=name)
+    else:
+        nvg = ob.vertex_groups[name]
+    nvg.add(v_list, weight, 'ADD')
+
+
+# vertex groups
+def get_vertex_weights(ob, group_name, default=1.0):
+    """Get weights of the group. if it's not
+    in the group set it to default"""
+    if group_name not in ob.vertex_groups:
+        ob.vertex_groups.new(name=group_name)
+
+    vertex_group = ob.vertex_groups[group_name]
+    all_vertices = range(len(ob.data.vertices))
+    weights = []
+
+    for idx in all_vertices:
+        try:
+            weight = vertex_group.weight(idx)
+            weights.append(weight)
+        except RuntimeError:
+            weights.append(default)
+
+    vertex_weights = np.array(weights, dtype=np.float32)
+    return vertex_weights
 
 
 #=======================#
@@ -96,6 +143,49 @@ def get_bmesh(ob=None, refresh=False, mesh=None, copy=False):
         return obm.copy()
 
     return obm
+
+
+# bmesh
+def get_tridex(ob, teidx=False, tmesh=False, tobm=None, refresh=False, cull_verts=None, free=False):
+    """Return an index for viewing the
+    verts as triangles using a mesh and
+    foreach_get.
+    Return tridex and triobm"""
+
+    if tobm is None:
+        tobm = get_bmesh(ob)
+        if cull_verts is not None:
+            tobm.verts.ensure_lookup_table()
+            cv = [tobm.verts[v] for v in cull_verts]
+            bmesh.ops.delete(tobm, geom=cv, context='VERTS')
+
+    bmesh.ops.triangulate(tobm, faces=tobm.faces)
+    me = bpy.data.meshes.new('tris')
+    tobm.to_mesh(me)
+    p_count = len(me.polygons)
+    tridex = np.empty((p_count, 3), dtype=D_TYPE_I)
+    me.polygons.foreach_get('vertices', tridex.ravel())
+    if free:
+        tobm.free()
+        return tridex
+    
+    if tmesh:
+        return tridex, tobm, me
+    if teidx:
+        eidx = np.array(me.edge_keys, dtype=D_TYPE_I)
+        # clear unused tri mesh
+        bpy.data.meshes.remove(me)
+        return tridex, eidx, tobm
+
+    # clear unused tri mesh
+    bpy.data.meshes.remove(me)
+
+    if refresh:
+        tobm.verts.ensure_lookup_table()
+        tobm.edges.ensure_lookup_table()
+        tobm.faces.ensure_lookup_table()
+
+    return tridex, tobm, None
 
 
 #=======================#
@@ -135,6 +225,24 @@ def oops(self, context):
     return
 
 
+# debug
+CT = None
+def timer():
+    return
+    
+    global CT
+    if CT is None:
+        CT = time.time()
+        return
+    frame = inspect.currentframe()
+    calling_line = frame.f_back.f_lineno
+    print()
+    print("line:", calling_line)
+    print(time.time() - CT)
+    CT = time.time()
+    print()
+
+
 #=======================#
 # BLENDER OBJECTS ------#
 #=======================#
@@ -142,6 +250,7 @@ def check_object(ob):
     return ob.name in bpy.context.scene.objects
 
 
+# blender objects
 def make_parent(ob1, ob2):
     """Make ob1 the child of ob2"""
     bpy.context.view_layer.update()
@@ -178,6 +287,7 @@ def link_mesh(verts, edges, faces, name='name', ob=None):
     return ob
 
 
+# blender objects
 def create_empty(display_type, name, world_matrix, scene=None):
     if display_type not in {'PLAIN_AXES', 'SINGLE_ARROW', 'CIRCLE', 'CUBE', 'SPHERE', 'CONE', 'ARROWS'}:
         print(f"Invalid display type: {display_type}")
@@ -198,6 +308,198 @@ def create_empty(display_type, name, world_matrix, scene=None):
 #=======================#
 # GEOMETRY -------------#
 #=======================#
+# geometry
+def prox_object(ob):
+    """Returns object including modifier and
+    shape key effects."""
+    dg = bpy.context.evaluated_depsgraph_get()
+    return ob.evaluated_get(dg)
+
+
+# geometry
+def absolute_co(ob, ar=None, world=True, prox=None):
+    """Get proxy vert coords in world space.
+    !!! Also returns prox object !!!
+    Might need view update before matrix."""
+
+    if prox is None:    
+        prox = prox_object(ob)
+
+    if ar is None:
+        count = len(prox.data.vertices)
+        ar = np.zeros((count, 3), dtype=D_TYPE_F)
+    prox.data.vertices.foreach_get("co", ar.ravel())
+    
+    if world:    
+        return apply_transforms(ob, ar), prox
+
+    return ar
+
+
+# geometry
+def co_to_shape(ob, co=None, key="current"):
+    if co is None:
+        co = get_co(ob)
+    ob.data.shape_keys.key_blocks[key].data.foreach_set('co', co.ravel())
+    ob.data.update()
+
+
+# geometry
+def get_shape_co_mode(ob=None, co=None, key='current'):
+    """Edit or object mode"""
+
+    if ob.data.is_editmode:
+        ob.update_from_editmode()
+
+    if co is None:
+        co = np.empty((len(ob.data.vertices), 3), dtype=np.float32)
+        
+    ob.data.shape_keys.key_blocks[key].data.foreach_get('co', co.ravel())
+    return co
+
+
+# geometry
+def get_shape_co(ob, shape='Key_1', co=None):
+    if co is None:
+        co = np.empty((len(ob.data.vertices), 3), dtype=D_TYPE_F)
+    ob.data.shape_keys.key_blocks[shape].data.foreach_get('co', co.ravel())
+    return co
+
+
+# geometry
+def set_shape_co(ob, shape, co):
+    ob.data.shape_keys.key_blocks[shape].data.foreach_set('co', co.ravel())
+
+
+# geometry
+def get_bmesh_co(obm, co=None):
+    """To get the grabbed location of a vertex we
+    need to get the data from the bmesh coords."""
+    if co is None:
+        return np.array([v.co for v in cloth.obm.verts], dtype=np.float32)
+
+    for i in range(co.shape[0]):
+        co[i] = obm.verts[i].co
+
+
+# geometry
+def get_co_edit(ob, ar=None, key='current'):
+    ob.update_from_editmode()
+    if ar is None:
+        c = len(ob.data.vertices)
+        ar = np.empty((c, 3), dtype=np.float32)
+    ob.data.shape_keys.key_blocks[key].data.foreach_get('co', ar.ravel())
+    return ar
+
+
+# geometry
+def get_co(ob):
+    """Returns Nx3 cooridnate set as numpy array"""
+    co = np.empty((len(ob.data.vertices), 3), dtype=D_TYPE_F)
+    ob.data.vertices.foreach_get("co", co.ravel())
+    return co
+
+
+# geometry
+def get_edge_centers(co, eidx, scale=0.5):
+    """Start and end of edge
+    has to match head and tail
+    of bone if center of mass
+    is not at 0.5."""
+    head = co[eidx[:, 0]]
+    vec = (co[eidx[:,1]] - head) * scale
+    return head + vec
+
+
+# geometry
+def slide_points_to_planes(e1, e2, origins, normals, intersect=False):
+    '''Takes the start and end of an edge.
+    Returns where it intersects the planes with a bool array for the
+    edges that pass through the plane'''
+    e_vecs = e2 - e1
+    e1ors = e1 - origins
+    edge_dots = np.einsum('ij,ij->i', e_vecs, normals)
+    dots = np.einsum('ij,ij->i', normals, e1ors)
+    scale = np.nan_to_num(dots / edge_dots)
+    
+    drop = (e1ors - e_vecs * scale[:, None]) + origins
+    if intersect:
+        intersect = (scale < 0) & (scale > -1)
+        return drop, intersect, scale
+    return drop
+
+
+# geometry
+def inside_triangles(tris, points, margin=0.0):  # , cross_vecs):
+    """Checks if points are inside triangles"""
+    origins = tris[:, 0]
+    cross_vecs = tris[:, 1:] - origins[:, None]
+
+    v2 = points - origins
+
+    # ---------
+    v0 = cross_vecs[:, 0]
+    v1 = cross_vecs[:, 1]
+
+    d00_d11 = np.einsum('ijk,ijk->ij', cross_vecs, cross_vecs)
+    d00 = d00_d11[:, 0]
+    d11 = d00_d11[:, 1]
+    d01 = np.einsum('ij,ij->i', v0, v1)
+    d02 = np.einsum('ij,ij->i', v0, v2)
+    d12 = np.einsum('ij,ij->i', v1, v2)
+
+    div = 1 / (d00 * d11 - d01 * d01)
+    u = (d11 * d02 - d01 * d12) * div
+    v = (d00 * d12 - d01 * d02) * div
+
+    w = 1 - (u + v)
+    # !!!! needs some thought
+    # margin = 0.0
+    # !!!! ==================
+    weights = np.array([w, u, v]).T
+    check = (u >= margin) & (v >= margin) & (w >= margin)
+    return check, weights
+
+
+# geometry
+def get_edge_bounds(edges):
+    """Takes an NxNx3 set of coords and returns
+    the min and max for each edge/tri"""
+    min = np.min(edges, axis=1)
+    max = np.max(edges, axis=1)
+    return [min, max]
+
+
+# geometry
+def get_tri_normals(tco, normalize=True):
+    """"""
+    tv1 = tco[:, 1] - tco[:, 0]
+    tv2 = tco[:, 2] - tco[:, 0]
+    cross = np.cross(tv1, tv2)
+    if normalize:    
+        return u_vecs(cross)    
+    return cross
+    
+
+# geometry
+def get_vertex_normals(ob):
+    """Vertex normals from object using blender
+    calculated normals."""
+    normals = np.zeros((len(ob.data.vertices), 3), dtype=D_TYPE_F)
+    ob.data.vertices.foreach_get('normal', normals.ravel())
+    return normals
+
+
+# geometry
+def xyz_world(ob, normalize=False):
+    """Get the xyz axis vectors of an object.
+    Assumes a scale of 1, 1, 1"""
+    xyz = np.array(ob.matrix_world, dtype=D_TYPE_F)[:3, :3].T
+    if normalize:
+        xyz /= np.array(ob.scale, dtype=np.float32)[:, None]
+    return xyz
+
+
 # geometry
 def apply_transforms(ob, co, update_view=False):
     """Get co in world space.
@@ -242,9 +544,52 @@ def revert_rotation(ob, co):
 
 
 # geometry
+def co_to_matrix_space(co, ob1, ob2):
+    """Takes the world coordinates of object
+    1 and puts them in object coordinate space"""
+        
+    local_matrix = np.linalg.inv(ob2.matrix_world) @ np.array(ob1.matrix_world, dtype=np.float32)
+    local_co = co @ local_matrix[i][:3, :3].T
+    local_co += local_matrix[i][:3, 3]
+    return local_co
+
+
+# geometry
 def u_vecs(vecs):
-    u_vecs = vecs / np.sqrt(np.einsum('ij,ij->i', vecs, vecs))[:, None]
-    return u_vecs
+    u_v = vecs / np.sqrt(np.einsum('ij,ij->i', vecs, vecs))[:, None]
+    return u_v
+
+
+# geometry
+def closest_points_edges(vecs, origins, p):
+    '''Returns the location of the points on the edge'''
+    vec2 = p - origins
+    d = np.einsum('ij,ij->i', vecs, vec2) / np.einsum('ij,ij->i', vecs, vecs)
+    cp = origins + vecs * d[:, None]
+    return cp, d
+
+
+# geometry
+def vec_to_vec_pivot(edges, vecs, factor=0.5):
+    """Rotate edges to match vecs respecting
+    a pivot defined by a factor that plots
+    the pivot along the edge. The factor
+    should be defined by the bone center
+    of mass.
+    !!! Assumes vecs are normalized !!!"""
+    
+    edge_vecs = (edges[:, 1] - edges[:, 0])
+    
+    rot_edges = np.empty_like(edges)
+    vecs_1 = edge_vecs * factor
+    vecs_2 = edge_vecs * (1 - factor)
+    edge_mid = edges[:, 0] + vecs_1
+    dist_1 = measure_vecs(vecs_1)[:, None]
+    dist_2 = measure_vecs(vecs_2)[:, None]
+
+    rot_edges[:, 0] = edge_mid - (vecs * dist_1)
+    rot_edges[:, 1] = edge_mid + (vecs * dist_2)
+    return rot_edges
 
 
 # geometry
@@ -272,22 +617,8 @@ def replot_orthagonal_mix(ph, use_cp=True):
     ### ===== MAKE X ORTHAGONAL TO Y ===== ###
     x_co = ph.current_co[ph.x_vidx]
     vecs = x_co[1::2] - x_co[::2]
-    u_vecs = C.u_vecs(vecs)
-    return u_vecs
-    
-    x_mid = ph.current_co[ph.mesh_bone_idx]
-    eco = ph.current_co[ph.y_eidx]
-    vecs = eco[:, 1] - eco[:, 0]
-    
-    cp, d = C.closest_points_edges(vecs=vecs, origins=x_mid, p=x_co[::2])
-    cp_vec = cp - x_co[::2]
-    ucp = C.u_vecs(cp_vec)
-
-    print(ph.x_vidx, "x vidx")
-
-    #ph.x_co[::2] = ph.x_mid - (ucp * ph.x_factor)
-    #ph.x_co[1::2] = ph.x_mid + (ucp * (1 - ph.x_factor))
-    return ucp
+    u_v = u_vecs(vecs)
+    return u_v
 
 
 def replot_orthagonal(ph, skip_mean=False, new_vecs=None):
@@ -312,9 +643,9 @@ def replot_orthagonal(ph, skip_mean=False, new_vecs=None):
     vecs = eco[:, 1] - eco[:, 0]
     
     if new_vecs is None:
-        cp, d = C.closest_points_edges(vecs=vecs, origins=ph.x_mid, p=ph.x_co[::2])
+        cp, d = closest_points_edges(vecs=vecs, origins=ph.x_mid, p=ph.x_co[::2])
         cp_vec = cp - ph.x_co[::2]
-        ucp = C.u_vecs(cp_vec)
+        ucp = u_vecs(cp_vec)
 
         ph.x_co[::2] = ph.x_mid - (ucp * ph.x_factor)
         ph.x_co[1::2] = ph.x_mid + (ucp * (1 - ph.x_factor))
@@ -326,24 +657,6 @@ def replot_orthagonal(ph, skip_mean=False, new_vecs=None):
 
 
 
-def code_line():
-    frame = inspect.currentframe()
-    calling_line = frame.f_back.f_lineno
-    print("Called on line:", calling_line)
+
 
     
-CT = None
-def timer():
-    return
-    
-    global CT
-    if CT is None:
-        CT = time.time()
-        return
-    frame = inspect.currentframe()
-    calling_line = frame.f_back.f_lineno
-    print()
-    print("line:", calling_line)
-    print(time.time() - CT)
-    CT = time.time()
-    print()
